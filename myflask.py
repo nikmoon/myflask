8b32691dc5dcb62e3be23c85d6e8ec04138cafc9
nikmoon <consensuss@rambler.ru>
2015-10-27 16:35:37 +0700

    Added project files

---
+++ myflask.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+from flup.server.fcgi import WSGIServer
+from flask import Flask, render_template, redirect, abort, url_for
+
+
+app = Flask(__name__)
+
+#def app(environ, start_response):
+#    start_response('200 OK', [('Content-Type', 'text/html')])
+#    return ['Hello World!\n', str(environ)]
+
+@app.route('/')
+def hello():
+    return redirect(url_for('hello_user'))
+
+@app.route('/test/')
+def test():
+    return os.getcwd()
+
+
+@app.route('/hello/')
+@app.route('/hello/<username>')
+def hello_user(username = None):
+    abort(401)
+    return render_template('hello.html', username = username)
+
+
+if __name__ == '__main__':
+    WSGIServer(app).run()
